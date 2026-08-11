bbc3aa39cebcdc605faf9b066526ff27d1dbf2ea
Prasanth B <prashphotographs@gmail.com>
2026-08-11 16:37:13 -0700

    latest changes using astro

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig, fontProviders } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	fonts: [
+		{
+			name: 'Playfair Display',
+			cssVariable: '--font-playfair',
+			provider: fontProviders.google(),
+			weights: [400, 500],
+			styles: ['normal'],
+			fallbacks: ['serif'],
+		},
+	],
+});
